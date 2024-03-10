93f8861addf72da6f325c8aa67ca23b9d4998cfb
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-10 14:33:18 -0500

    create busy poll threads

--- quickassist/lookaside/access_layer/src/sample_code/performance/include/test_vf_perf_utils.h
+++ quickassist/lookaside/access_layer/src/sample_code/performance/include/test_vf_perf_utils.h
@@ -1,5 +1,9 @@
 #ifndef _TEST_VF_PERF_UTILS
 #define _TEST_VF_PERF_UTILS
+extern CpaInstanceHandle *dcInstances_g;
+extern Cpa16U numDcInstances_g;
+extern CpaInstanceInfo2 *instanceInfo2;
+
 static void freeDcBufferList(CpaBufferList **buffListArray,
                              Cpa32U numberOfBufferList)
 {
@@ -31,6 +35,37 @@ static void freeDcBufferList(CpaBufferList **buffListArray,
             qaeMemFreeNUMA((void **)&buffListArray[i]->pPrivateMetaData);
         }
     }
+}
+
+
+void busyPollFn(){
+
+}
+
+void epollFn(){
+    
+}
+
+
+CpaStatus createEpollThreads(){
+  int fd = -1;
+  CpaInstanceInfo2 *instanceInfo2 = NULL;
+  Cpa16U i = 0, j = 0, numCreatedPollingThreads = 0;
+  Cpa32U coreAffinity = 0;
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  status = icp_sal_DcGetFileDescriptor(dcInstances_g[i], &fd);
+  if (status != CPA_STATUS_SUCCESS)
+  {
+      PRINT_ERR("icp_sal_DcGetFileDescriptor failed\n");
+      return status;
+  }
+}
+
+CpaStatus createBusyPollThreads(){
+  int fd = -1;
+  CpaInstanceInfo2 *instanceInfo2 = NULL;
+  Cpa16U i = 0, j = 0, numCreatedPollingThreads = 0;
+  Cpa32U coreAffinity = 0;
 }
 
 #endif
