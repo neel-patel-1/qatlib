63216afd0936974babae170ab2eaaf00b0f8811d
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-12 20:40:20 -0500

    polling sampling vars

--- quickassist/lookaside/access_layer/src/sample_code/performance/common/qat_perf_latency.c
+++ quickassist/lookaside/access_layer/src/sample_code/performance/common/qat_perf_latency.c
@@ -2,38 +2,38 @@
  *
  * This file is provided under a dual BSD/GPLv2 license.  When using or
  *   redistributing this file, you may do so under either license.
- * 
+ *
  *   GPL LICENSE SUMMARY
- * 
+ *
  *   Copyright(c) 2007-2022 Intel Corporation. All rights reserved.
- * 
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of version 2 of the GNU General Public License as
  *   published by the Free Software Foundation.
- * 
+ *
  *   This program is distributed in the hope that it will be useful, but
  *   WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *   General Public License for more details.
- * 
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  *   The full GNU General Public License is included in this distribution
  *   in the file called LICENSE.GPL.
- * 
+ *
  *   Contact Information:
  *   Intel Corporation
- * 
+ *
  *   BSD LICENSE
- * 
+ *
  *   Copyright(c) 2007-2022 Intel Corporation. All rights reserved.
  *   All rights reserved.
- * 
+ *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
  *   are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -43,7 +43,7 @@
  *     * Neither the name of Intel Corporation nor the names of its
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
- * 
+ *
  *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -55,8 +55,8 @@
  *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * 
+ *
+ *
  *
  ***************************************************************************/
 #include "cpa_dc.h"
@@ -203,6 +203,8 @@ CpaStatus qatInitLatency(perf_data_t *performanceStats,
                     (numberOfLists * numberOfLoops) / MAX_LATENCY_COUNT;
                 performanceStats->nextCount = performanceStats->countIncrement;
                 performanceStats->latencyCount = 0;
+                performanceStats->pollLatencyCount = 0;
+                performanceStats->submissionLatencyCount = 0;
                 performanceStats->response_times =
                     qaeMemAlloc(sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
                 QAT_PERF_CHECK_NULL_POINTER_AND_UPDATE_STATUS(
@@ -211,8 +213,16 @@ CpaStatus qatInitLatency(perf_data_t *performanceStats,
                 {
                     performanceStats->start_times = qaeMemAlloc(
                         sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
+                    performanceStats->req_sub_start_times = qaeMemAlloc(
+                        sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
+                    performanceStats->req_sub_end_times = qaeMemAlloc(
+                        sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
                     QAT_PERF_CHECK_NULL_POINTER_AND_UPDATE_STATUS(
                         performanceStats->start_times, status);
+                    performanceStats->poll_start_times = qaeMemAlloc(
+                        sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
+                    performanceStats->poll_end_times = qaeMemAlloc(
+                        sizeof(perf_cycles_t) * MAX_LATENCY_COUNT + 1);
                 }
             }
         }
@@ -242,6 +252,65 @@ CpaStatus qatStartLatencyMeasurement(perf_data_t *performanceStats,
                 performanceStats->start_times[i] = sampleCodeTimestamp();
             }
         }
+    }
+    return status;
+}
+
+CpaStatus requestSubmitStart(perf_data_t *perf_data)
+{
+    CpaStatus status = CPA_STATUS_SUCCESS;
+    if(latency_enable){
+        if(perf_data->submissionLatencyCount <= MAX_LATENCY_COUNT)
+            perf_data->req_sub_start_times[perf_data->submissionLatencyCount]
+                = sampleCodeTimestamp();
+        else
+            status = CPA_STATUS_FAIL;
+    }
+    else{
+        status = CPA_STATUS_FAIL;
+    }
+    return status;
+}
+CpaStatus requestSubmitStop(perf_data_t *perf_data)
+{
+    CpaStatus status = CPA_STATUS_SUCCESS;
+    if(latency_enable){
+        if(perf_data->submissionLatencyCount <= MAX_LATENCY_COUNT)
+            perf_data->req_sub_end_times[perf_data->submissionLatencyCount] = sampleCodeTimestamp();
+        else
+            status = CPA_STATUS_FAIL;
+    }
+    else{
+        status = CPA_STATUS_FAIL;
+    }
+    return status;
+}
+
+CpaStatus startPollLatency(perf_data_t *perf_data){
+    CpaStatus status = CPA_STATUS_SUCCESS;
+    if(latency_enable){
+        if(perf_data->pollLatencyCount <= MAX_LATENCY_COUNT)
+            perf_data->poll_start_times[perf_data->pollLatencyCount]
+                = sampleCodeTimestamp();
+        else
+            status = CPA_STATUS_FAIL;
+    }
+    else{
+        status = CPA_STATUS_FAIL;
+    }
+    return status;
+}
+CpaStatus stopPollLatency(perf_data_t *perf_data){
+    CpaStatus status = CPA_STATUS_SUCCESS;
+    if(latency_enable){
+        if(perf_data->pollLatencyCount <= MAX_LATENCY_COUNT)
+            perf_data->poll_end_times[perf_data->pollLatencyCount]
+                = sampleCodeTimestamp();
+        else
+            status = CPA_STATUS_FAIL;
+    }
+    else{
+        status = CPA_STATUS_FAIL;
     }
     return status;
 }
@@ -303,6 +372,10 @@ CpaStatus qatLatencyPollForResponses(perf_data_t *performanceStats,
                     }
                 }
             }
+            else{
+                PRINT_ERR("Not Sync\n");
+                exit(-1);
+            }
         }
     }
     return status;
@@ -332,6 +405,10 @@ CpaStatus qatSummariseLatencyMeasurements(perf_data_t *performanceStats)
                 perf_cycles_t latency = performanceStats->response_times[i] -
                                         performanceStats->start_times[i];
                 performanceStats->aveLatency += latency;
+                performanceStats->aveSubmissionLatency +=
+                    performanceStats->req_sub_end_times[i] - performanceStats->req_sub_start_times[i];
+                performanceStats->avePollingLatency +=
+                    performanceStats->poll_end_times[i] - performanceStats->poll_start_times[i];
 
                 if (latency < performanceStats->minLatency)
                     performanceStats->minLatency = latency;
@@ -345,6 +422,10 @@ CpaStatus qatSummariseLatencyMeasurements(perf_data_t *performanceStats)
                  * This will be reported by dcPrintStats() at the end of the
                  * test */
                 do_div(performanceStats->aveLatency,
+                       performanceStats->latencyCount);
+                do_div(performanceStats->aveSubmissionLatency,
+                       performanceStats->latencyCount);
+                do_div(performanceStats->avePollingLatency,
                        performanceStats->latencyCount);
             }
             status = qatFreeLatency(performanceStats);
