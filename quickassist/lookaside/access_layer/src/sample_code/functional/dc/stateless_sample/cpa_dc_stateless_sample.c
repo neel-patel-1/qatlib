85ccbd7e6e5a5beb65940d105bbbc5f143ad5fb3
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-19 19:31:00 -0500

    glb timestamps

--- quickassist/lookaside/access_layer/src/sample_code/functional/dc/stateless_sample/cpa_dc_stateless_sample.c
+++ quickassist/lookaside/access_layer/src/sample_code/functional/dc/stateless_sample/cpa_dc_stateless_sample.c
@@ -2,38 +2,38 @@
  *
  * This file is provided under a dual BSD/GPLv2 license.  When using or
  *   redistributing this file, you may do so under either license.
- * 
+ *
  *   GPL LICENSE SUMMARY
- * 
+ *
  *   Copyright(c) 2007-2022 Intel Corporation. All rights reserved.
- * 
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of version 2 of the GNU General Public License as
  *   published by the Free Software Foundation.
- * 
+ *
  *   This program is distributed in the hope that it will be useful, but
  *   WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *   General Public License for more details.
- * 
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  *   The full GNU General Public License is included in this distribution
  *   in the file called LICENSE.GPL.
- * 
+ *
  *   Contact Information:
  *   Intel Corporation
- * 
+ *
  *   BSD LICENSE
- * 
+ *
  *   Copyright(c) 2007-2022 Intel Corporation. All rights reserved.
  *   All rights reserved.
- * 
+ *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
  *   are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -43,7 +43,7 @@
  *     * Neither the name of Intel Corporation nor the names of its
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
- * 
+ *
  *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -55,8 +55,8 @@
  *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * 
+ *
+ *
  *
  ***************************************************************************/
 
@@ -72,6 +72,8 @@
 #include "cpa_sample_utils.h"
 
 extern int gDebugParam;
+uint64_t *g_ts;
+uint64_t *response_ts;
 
 #define SAMPLE_MAX_BUFF 1024
 #define TIMEOUT_MS 5000 /* 5 seconds */
@@ -148,6 +150,8 @@ CpaStatus dcStatelessSample(void);
 static void dcCallback(void *pCallbackTag, CpaStatus status)
 {
     PRINT_DBG("Callback called with status = %d.\n", status);
+    int index = *(int)pCallbackTag;
+    g_ts[index] = sampleCoderdtsc();
 
     if (NULL != pCallbackTag)
     {
@@ -493,6 +497,9 @@ CpaStatus dcStatelessSample(void)
     CpaDcSessionSetupData sd = {0};
     CpaDcStats dcStats = {0};
 
+    uint64_t numReqResponses = 10000;
+    g_ts = (uint64_t *)malloc(sizeof(uint64_t) * 2);
+    response_ts = (uint64_t *)malloc(sizeof(uint64_t) * 2);
     /*
      * In this simplified version of instance discovery, we discover
      * exactly one instance of a data compression service.
