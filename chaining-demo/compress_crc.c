a0636ee90df0a20237d3de4d50e41d055ca2ad2d
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-05-08 13:01:51 -0500

    new stream for each comp

--- chaining-demo/compress_crc.c
+++ chaining-demo/compress_crc.c
@@ -419,6 +419,43 @@ void createCompressCrc64Poller(CpaInstanceHandle dcInstHandle, Cpa16U id, pthrea
   }
 }
 
+/* resets the stream after each compression */
+CpaStatus deflateCompressAndTimestamp(
+  CpaBufferList *pBufferListSrc,
+  CpaBufferList *pBufferListDst, CpaDcRqResults *pDcResults, int index, callback_args *cb_args
+){
+  z_stream strm;
+  int ret;
+  memset(&strm, 0, sizeof(z_stream));
+  ret = deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  packet_stats *stats = cb_args->stats[index];
+
+  Cpa64U submitTime = sampleCoderdtsc();
+  stats->submitTime = submitTime;
+
+  Cpa8U *src = pBufferListSrc->pBuffers[0].pData;
+  Cpa32U srcLen = pBufferListSrc->pBuffers->dataLenInBytes;
+  Cpa8U *dst = pBufferListDst->pBuffers[0].pData;
+  Cpa32U dstLen = pBufferListDst->pBuffers->dataLenInBytes;
+
+  strm.avail_in = srcLen;
+  strm.next_in = (Bytef *)src;
+  strm.avail_out = dstLen;
+  strm.next_out = (Bytef *)dst;
+  ret = deflate(&strm, Z_FINISH);
+  if(ret != Z_STREAM_END)
+  {
+    fprintf(stderr, "Error in deflate, ret = %d\n", ret);
+    return CPA_STATUS_FAIL;
+  }
+  pDcResults->produced = strm.total_out;
+
+  stats->receiveTime = sampleCoderdtsc();
+
+  return status;
+}
+
 void createCompressCrc64Submitter(
   CpaInstanceHandle dcInstHandle,
   CpaDcSessionHandle sessionHandle,
@@ -523,7 +560,7 @@ int main(){
   dcInstHandle = dcInstHandles[0];
 
   /* Get test buffers and perf stats setup */
-  Cpa32U numOperations = 2;
+  Cpa32U numOperations = 1000;
   Cpa32U bufferSize = 1024;
   CpaDcOpData **opData = NULL;
   CpaDcRqResults **dcResults = NULL;
@@ -552,9 +589,7 @@ int main(){
 
   int ret;
 
-  z_stream strm;
-  memset(&strm, 0, sizeof(z_stream));
-  ret = deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+
   if(ret != Z_OK)
   {
     fprintf(stderr, "Error in deflateInit, ret = %d\n", ret);
@@ -564,30 +599,35 @@ int main(){
   for(int i=0; i<numOperations; i++){
     stats[i]->submitTime = sampleCoderdtsc();
 
-    Cpa8U *src = srcBufferLists[i]->pBuffers[0].pData;
-    Cpa32U srcLen = srcBufferLists[i]->pBuffers->dataLenInBytes;
-    Cpa8U *dst = dstBufferLists[i]->pBuffers[0].pData;
-    Cpa32U dstLen = dstBufferLists[i]->pBuffers->dataLenInBytes;
-
-    strm.avail_in = srcLen;
-    strm.next_in = (Bytef *)src;
-    strm.avail_out = dstLen;
-    strm.next_out = (Bytef *)dst;
-    ret = deflate(&strm, Z_FINISH);
-    if(ret != Z_STREAM_END)
-    {
-      fprintf(stderr, "Error in deflate, ret = %d\n", ret);
-      return CPA_STATUS_FAIL;
+    if(CPA_STATUS_SUCCESS != deflateCompressAndTimestamp(
+      srcBufferLists[i], dstBufferLists[i], dcResults[i], i, cb_args[i])){
+      PRINT_ERR("Error in compress data on %d'th packet\n", i);
     }
-    dcResults[i]->produced = strm.total_out;
 
-    stats[i]->receiveTime = sampleCoderdtsc();
-    ret = deflateReset(&strm);
-    if(ret != Z_OK)
-    {
-      fprintf(stderr, "Error in deflateReset, ret = %d\n", ret);
-      return CPA_STATUS_FAIL;
-    }
+    // Cpa8U *src = srcBufferLists[i]->pBuffers[0].pData;
+    // Cpa32U srcLen = srcBufferLists[i]->pBuffers->dataLenInBytes;
+    // Cpa8U *dst = dstBufferLists[i]->pBuffers[0].pData;
+    // Cpa32U dstLen = dstBufferLists[i]->pBuffers->dataLenInBytes;
+
+    // strm.avail_in = srcLen;
+    // strm.next_in = (Bytef *)src;
+    // strm.avail_out = dstLen;
+    // strm.next_out = (Bytef *)dst;
+    // ret = deflate(&strm, Z_FINISH);
+    // if(ret != Z_STREAM_END)
+    // {
+    //   fprintf(stderr, "Error in deflate, ret = %d\n", ret);
+    //   return CPA_STATUS_FAIL;
+    // }
+    // dcResults[i]->produced = strm.total_out;
+
+    // stats[i]->receiveTime = sampleCoderdtsc();
+    // ret = deflateReset(&strm);
+    // if(ret != Z_OK)
+    // {
+    //   fprintf(stderr, "Error in deflateReset, ret = %d\n", ret);
+    //   return CPA_STATUS_FAIL;
+    // }
   }
 
   printStats(stats, numOperations, bufferSize);
