83eccb670009f462704af09cb414db78637f0d82
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-05-12 00:17:42 -0500

    submit comp+crc

--- chaining-demo/compress_crc.c
+++ chaining-demo/compress_crc.c
@@ -383,15 +383,139 @@ CpaStatus functionalSymTest(){
   return CPA_STATUS_SUCCESS;
 }
 
-int main(){
-  CpaStatus status = CPA_STATUS_SUCCESS, stat;
+int main()
+{
+  CpaStatus stat;
 
   stat = qaeMemInit();
   stat = icp_sal_userStartMultiProcess("SSL", CPA_FALSE);
 
-  int numOperations = 1000;
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  Cpa8U *pBufferMetaDst2 = NULL;
+  Cpa32U bufferMetaSize = 0;
+  CpaBufferList *pBufferListSrc = NULL;
+  CpaBufferList *pBufferListDst = NULL;
+  CpaBufferList *pBufferListDst2 = NULL;
+  CpaDcOpData opData = {};
+  Cpa8U *sampleData = NULL;
+  Cpa32U sampleDataSize = 512;
+  CpaDcInstanceCapabilities cap = {0};
+  CpaDcHuffType huffType = CPA_DC_HT_FULL_DYNAMIC;
+  if(CPA_STATUS_SUCCESS != prepareSampleBuffer((Cpa8U **)&sampleData, sampleDataSize)){
+    fprintf(stderr, "Failed to prepare sample buffer\n");
+    return CPA_STATUS_FAIL;;
+  }
 
-  functionalSymTest();
+  Cpa32U bufferSize = sampleDataSize;
+  Cpa32U dstBufferSize = bufferSize;
+  Cpa64U checksum = 0;
+
+  CpaDcRqResults dcResults;
+  struct COMPLETION_STRUCT complete;
+  INIT_OPDATA(&opData, CPA_DC_FLUSH_FINAL);
+
+  CpaInstanceHandle dcInstHandles[MAX_INSTANCES];
+  CpaDcSessionHandle sessionHandles[MAX_INSTANCES];
+  Cpa16U numInstances = 1;
+  int numOperations = 1000;
+  int numFlows = 1; /* Spawn a crc poller to encrypt fwder */
+
+
+  prepareMultipleCompressAndCrc64InstancesAndSessions(dcInstHandles, sessionHandles, numInstances, numFlows);
+
+  CpaInstanceHandle dcInstHandle = dcInstHandles[0];
+  CpaDcSessionHandle sessionHandle = sessionHandles[0];
+
+  status =
+    createSourceBufferList(&pBufferListSrc, sampleData, sampleDataSize, dcInstHandle, huffType);
+
+  status =
+    createDstBufferList(&pBufferListDst, sampleDataSize, dcInstHandle, huffType);
+
+  if(CPA_STATUS_SUCCESS != status){
+    fprintf(stderr, "Failed to create source buffer list\n");
+    return CPA_STATUS_FAIL;;
+  }
+
+
+  /*
+    * Now, we initialize the completion variable which is used by the
+    * callback
+    * function to indicate that the operation is complete.  We then perform
+    * the
+    * operation.
+    */
+  PRINT_DBG("cpaDcCompressData2\n");
+
+  //<snippet name="perfOp">
+  COMPLETION_INIT(&complete);
+
+  /* enable integrityCrcCheck */
+  cpaDcQueryCapabilities(dcInstHandle, &cap);
+  CpaCrcData crcData = {0};
+  if(cap.integrityCrcs64b == CPA_TRUE ){
+    PRINT_DBG("Integrity CRC is enabled\n");
+    opData.integrityCrcCheck = CPA_TRUE;
+    opData.pCrcData = &crcData;
+  }
+
+  status = cpaDcCompressData2(
+      dcInstHandle,
+      sessionHandle,
+      pBufferListSrc,     /* source buffer list */
+      pBufferListDst,     /* destination buffer list */
+      &opData,            /* Operational data */
+      &dcResults,         /* results structure */
+      (void *)&complete); /* data sent as is to the callback function*/
+                              //</snippet>
+
+  if (CPA_STATUS_SUCCESS != status)
+  {
+      PRINT_ERR("cpaDcCompressData2 failed. (status = %d)\n", status);
+  }
+
+  /*
+    * We now wait until the completion of the operation.  This uses a macro
+    * which can be defined differently for different OSes.
+    */
+  if (CPA_STATUS_SUCCESS == status)
+  {
+      if (!COMPLETION_WAIT(&complete, 5000))
+      {
+          PRINT_ERR("timeout or interruption in cpaDcCompressData2\n");
+          status = CPA_STATUS_FAIL;
+      }
+  }
+
+  /*
+    * We now check the results
+    */
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    if (dcResults.status != CPA_DC_OK)
+    {
+        PRINT_ERR("Results status not as expected (status = %d)\n",
+                  dcResults.status);
+        status = CPA_STATUS_FAIL;
+    }
+    else
+    {
+        PRINT_DBG("Data consumed %d\n", dcResults.consumed);
+        PRINT_DBG("Data produced %d\n", dcResults.produced);
+        if(cap.integrityCrcs64b == CPA_TRUE){
+          PRINT_DBG("Uncompressed CRC64 0x%lx\n", crcData.integrityCrc64b.iCrc);
+          PRINT_DBG("Compressed CRC64 0x%lx\n", crcData.integrityCrc64b.oCrc);
+        } else {
+          PRINT_DBG("Adler checksum 0x%x\n", dcResults.checksum);
+        }
+    }
+    /* To compare the checksum with decompressed output */
+    checksum = crcData.integrityCrc64b.iCrc;
+  }
+
+
+  COMPLETION_DESTROY(&complete);
+
 
 
   icp_sal_userStop();
