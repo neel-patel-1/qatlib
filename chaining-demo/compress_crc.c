e5ec112efcbb13e1eac2565fd212c33972bea95b
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-05-27 20:21:00 -0500

    allocate the dsa and the hw_descs

--- chaining-demo/compress_crc.c
+++ chaining-demo/compress_crc.c
@@ -51,11 +51,15 @@ int gDebugParam = 1;
 
 #define NUMCONTEXTS 10              /* how many contexts to make */
 #define STACKSIZE 4096
+#define NUM_DESCS 128 /* Size of the shared work queue */
 
 extern int swapcontext_very_fast(ucontext_t *ouctx, ucontext_t *uctx);
 ucontext_t *cur_context;            /* a pointer to the current_context */
 int curcontext = 0;
 ucontext_t contexts[NUMCONTEXTS];   /* store our context info */
+
+struct acctest_context *dsa = NULL;
+struct hw_desc *descs = NULL;
 
 void
 mkcontext(ucontext_t *uc,  void *function)
@@ -82,31 +86,66 @@ mkcontext(ucontext_t *uc,  void *function)
     /* setup the function we're going to, and n-1 arguments. */
     makecontext(uc, function, 1);
 
-    printf("context is %p\n", uc);
+    PRINT_DBG("context is %p\n", uc);
 }
 
 void
 scheduler()
 {
-    printf("scheduling out thread %d\n", curcontext);
+    PRINT_DBG("scheduling out thread %d\n", curcontext);
 
     curcontext = (curcontext + 1) % NUMCONTEXTS; /* round robin */
     cur_context = &contexts[curcontext];
 
-    printf("scheduling in thread %d\n", curcontext);
+    PRINT_DBG("scheduling in thread %d\n", curcontext);
 
     setcontext(cur_context); /* go */
 }
 
-void thread(){
-  printf("Thread created\n");
+void worker_fn(){
+  /* busy-while loop polls for responses and swaps the context */
+
+}
+
+void request_fn(){
+  /* App work */
+
+  /* Submit request and yield */
+
+
+  /* Re-entry point */
+
+}
+
+void requestGen(){
+  /* Generate pre-processed Requests to feed to the worker */
 }
 
 
 int main(){
 
+  /* The requests all have access to the same accel */
 
-  mkcontext(&contexts[0], thread);
+  int tflags = TEST_FLAGS_BOF;
+  int rc;
+  int wq_type = ACCFG_WQ_SHARED;
+  int dev_id = 0;
+  int wq_id = 0;
+  int opcode = 16;
+  dsa = acctest_init(tflags);
+  dsa->dev_type = ACCFG_DEVICE_DSA;
+  if (!dsa)
+		return -ENOMEM;
+
+  rc = acctest_alloc(dsa, wq_type, dev_id, wq_id);
+	if (rc < 0)
+		return -ENOMEM;
+
+  /* Allocate the request set we will use */
+  descs = malloc(sizeof(struct hw_desc) * NUM_DESCS);
+
+  mkcontext(&contexts[1], request_fn);
+  setcontext(&contexts[1]);
 
 exit:
 
