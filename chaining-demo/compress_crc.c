7b445237438de3ee83057d74be0a840189579003
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-05-27 19:34:13 -0500

    mkcontext and swapcontext predef

--- chaining-demo/compress_crc.c
+++ chaining-demo/compress_crc.c
@@ -34,16 +34,57 @@
 
 #include <xmmintrin.h>
 
-#include <ucontext.h>
-
-
 
 int gDebugParam = 1;
-#define NUMCONTEXTS 10              /* how many contexts to make */
 
+
+#include <ucontext.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <poll.h>
+
+
+
+#define NUMCONTEXTS 10              /* how many contexts to make */
+#define STACKSIZE 4096
+
+extern int swapcontext_very_fast(ucontext_t *ouctx, ucontext_t *uctx);
 ucontext_t *cur_context;            /* a pointer to the current_context */
 int curcontext = 0;
 ucontext_t contexts[NUMCONTEXTS];   /* store our context info */
+
+void
+mkcontext(ucontext_t *uc,  void *function)
+{
+    void * stack;
+
+    getcontext(uc);
+
+    stack = malloc(STACKSIZE);
+    if (stack == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+    /* we need to initialize the ucontext structure, give it a stack,
+        flags, and a sigmask */
+    uc->uc_stack.ss_sp = stack;
+    uc->uc_stack.ss_size = STACKSIZE;
+    uc->uc_stack.ss_flags = 0;
+    if (sigemptyset(&uc->uc_sigmask) < 0){
+      perror("sigemptyset");
+      exit(1);
+    }
+
+    /* setup the function we're going to, and n-1 arguments. */
+    makecontext(uc, function, 1);
+
+    printf("context is %p\n", uc);
+}
+
 void
 scheduler()
 {
@@ -57,11 +98,15 @@ scheduler()
     setcontext(cur_context); /* go */
 }
 
+void thread(){
+  printf("Thread created\n");
+}
+
 
 int main(){
 
 
-
+  mkcontext(&contexts[0], thread);
 
 exit:
 
