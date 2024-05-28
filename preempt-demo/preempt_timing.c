89620e41f0d9c4a2964fb125789272fb130691ce
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-05-28 12:07:42 -0500

    submit from request context wait on main context

--- preempt-demo/preempt_timing.c
+++ preempt-demo/preempt_timing.c
@@ -26,10 +26,26 @@ ucontext_t contexts[NUMCONTEXTS];   /* store our context info */
 char *src_bufs[NUM_BUFS];
 char *dst_bufs[NUM_BUFS];
 int curbuf = 0;
+int buf_size = BUF_SIZE;
 
-struct task_node *tsk_node;
+struct task_node *sched_tsk_node;
+struct task_node *req_tsk_node;
 
-const volatile uint8_t *next_comp;
+struct acctest_context *dsa = NULL;
+const volatile uint8_t *comp;
+struct hw_desc *hw;
+
+void request_func(){
+  for(int i=0; i<buf_size; i++){
+    src_bufs[curbuf][i] = (char)i;
+  }
+
+  prepare_memcpy_task(req_tsk_node->tsk, dsa, (src_bufs[0]), buf_size, dst_bufs[0]);
+  hw = req_tsk_node->tsk->desc;
+  if( enqcmd(dsa->wq_reg, hw) ){PRINT_ERR("Failure in enq\n"); exit(-1);};
+  comp = (uint8_t *)req_tsk_node->tsk->comp;
+
+}
 
 
 void
@@ -65,17 +81,17 @@ mkcontext(ucontext_t *uc,  void *function)
 int main(){
 
   /* The requests all have access to the same accel */
-  struct acctest_context *dsa = NULL;
   int tflags = TEST_FLAGS_BOF;
-  int buf_size = BUF_SIZE;
   int numDescs = NUM_DESCS;
 
   struct hw_desc *hw = NULL;
-  const volatile uint8_t *comp;
+  struct task_node *tsk_node = NULL;
 
   allocDsa(&dsa);
   allocTasks(dsa, &tsk_node, DSA_OPCODE_MEMMOVE, buf_size, tflags, numDescs);
 
+  req_tsk_node = tsk_node;
+  sched_tsk_node = tsk_node;
 
   /* Allocate the buffers we will use */
   for(int i=0; i<NUM_BUFS; i++){
@@ -83,22 +99,17 @@ int main(){
     dst_bufs[i] = malloc(BUF_SIZE);
   }
 
-  for(int i=0; i<buf_size; i++){
-    src_bufs[0][i] = (char)i;
-  }
-  prepare_memcpy_task(tsk_node->tsk, dsa, (src_bufs[0]), buf_size, dst_bufs[0]);
-  hw = tsk_node->tsk->desc;
-  comp = (uint8_t *)tsk_node->tsk->comp;
-  next_comp = comp;
-  if( enqcmd(dsa->wq_reg, hw) ){PRINT_ERR("Failure in enq\n"); exit(-1);};
-  while(*next_comp == 0){}
+  mkcontext(&contexts[0], request_func);
+  setcontext(&contexts[0]);
+
+  comp = (uint8_t *)sched_tsk_node->tsk->comp;
+  while(*comp == 0){}
   if(memcmp(src_bufs[0], dst_bufs[0], buf_size) != 0){
     PRINT_ERR("Failed copy\n");
   }
 
   /* Allocate the completion records */
 
-  // mkcontext(&contexts[1], request_fn);
 
   // cur_context = &(contexts[1]);
   // setcontext(&contexts[1]);
