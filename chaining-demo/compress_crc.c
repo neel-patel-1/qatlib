f5d3e4d3315a659f2be2ee3e7e42e0110a18f38a
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-06-07 13:32:18 -0500

    l1 seq

--- chaining-demo/compress_crc.c
+++ chaining-demo/compress_crc.c
@@ -2151,7 +2151,12 @@ void yield_offload_request_ts (fcontext_transfer_t arg) {
     }
     ts13[idx] = sampleCoderdtsc();
     /* perform accesses */
-    chase_pointers(dst, numAccesses);
+    for(int i=0; i<memSize; i+=4096){
+      if(((uint8_t *)dst)[i] != ((uint8_t *)src)[i]){
+        PRINT_ERR("Payload mismatch: 0x%x 0x%x\n", dst[i], src[i]);
+        // return -EINVAL;
+      }
+    }
     /* returning control to the scheduler */
     ts14[idx] = sampleCoderdtsc();
     fcontext_swap(parent, NULL);
