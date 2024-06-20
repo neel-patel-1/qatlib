25f13b245a05fff64abc0b9d548e7baa89d437df
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-06-20 16:13:48 -0500

    10,000 prefetch and block iterations

--- parse_prefetch.sh
+++ parse_prefetch.sh
@@ -2,5 +2,12 @@
 
 
 make -j
-sudo perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses taskset -c 30 ./compress_crc block | tee block.log
-sudo perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses taskset -c 30 ./compress_crc prefetch | tee prefetch.log
+# sudo perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses taskset -c 30 ./compress_crc block | tee block.log
+sudo taskset -c 30 ./compress_crc block | tee block.log
+# # sudo perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses taskset -c 30 ./compress_crc prefetch | tee prefetch.log
+sudo taskset -c 30 ./compress_crc prefetch | tee prefetch.log
+
+echo -n "Block "
+grep RequestOnCPU block.log  | awk '{print $2}'
+echo -n "Prefetch "
+grep RequestOnCPU prefetch.log  | awk '{print $2}'
