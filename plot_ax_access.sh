53a8bb488dddc3d2f3a2d84a143b182175df5a57
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-06-24 01:28:30 -0500

    16kb

--- plot_ax_access.sh
+++ plot_ax_access.sh
@@ -5,9 +5,18 @@ make -j
 
 echo | tee log
 
+size=16384
 
 # blocking ax payload access (strided) on a 4kb buffer
-sudo taskset -c 30  stdbuf -o0 ./compress_crc -a -t 0 -s 4096 -i 1000 -b | tee -a  log
+sudo taskset -c 30  stdbuf -o0 ./compress_crc -a -t 0 -s $size -i 1000 -b | tee -a  log
 
 # blocking host payload access (strided) on a 4kb buffer
-sudo taskset -c 30  stdbuf -o0 ./compress_crc -t 0 -s 4096 -i 1000 -b | tee -a  log
+sudo taskset -c 30  stdbuf -o0 ./compress_crc -t 0 -s $size -i 1000 -b | tee -a  log
+
+# blocking ax payload access (random) on a 4kb buffer
+sudo taskset -c 30  stdbuf -o0 ./compress_crc -a -t 1 -s $size -i 1000 -b | tee -a  log
+
+# blocking host payload access (random) on a 4kb buffer
+sudo taskset -c 30  stdbuf -o0 ./compress_crc -t 1 -s $size -i 1000 -b | tee -a  log
+
+grep Cycles log | awk '{print $20}'
