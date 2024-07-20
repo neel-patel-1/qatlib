11e86df2cd44900fdc231a8d4718cdc33e381cb9
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-19 21:26:00 -0500

    construct linked list

--- ax_access_overhead_measurement/walker_ax_access_overhead.cpp
+++ ax_access_overhead_measurement/walker_ax_access_overhead.cpp
@@ -1,5 +1,18 @@
 #include "dsa_alloc.h"
 
+#include <cstdlib>
+
+extern "C"{
+  #include "linked_list.h"
+}
+
+
+bool gDebugParam = true;
 int main(){
 
+  node *src_node_1 = (node *)malloc(sizeof(node));
+  node *dst_node_2 = (node *)malloc(sizeof(node));
+
+  dsa_llc_realloc(NULL, NULL, 0);
+  return 0;
 }
