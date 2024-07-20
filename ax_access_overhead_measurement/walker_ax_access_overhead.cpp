f431a5eb50456590336768602847f17732a11747
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-19 21:47:05 -0500

    src node practice

--- ax_access_overhead_measurement/walker_ax_access_overhead.cpp
+++ ax_access_overhead_measurement/walker_ax_access_overhead.cpp
@@ -2,17 +2,33 @@
 
 #include <cstdlib>
 
-extern "C"{
-  #include "linked_list.h"
-}
 
+
+typedef struct _node{
+  uint32_t docID;
+  struct _node *next;
+} node;
 
 bool gDebugParam = true;
 int main(){
 
   node *src_node_1 = (node *)malloc(sizeof(node));
+  node *src_node_2 = (node *)malloc(sizeof(node));
+  node *dst_node_1 = (node *)malloc(sizeof(node));
   node *dst_node_2 = (node *)malloc(sizeof(node));
 
-  dsa_llc_realloc(NULL, NULL, 0);
+  node *head = src_node_1;
+
+  src_node_1->next = dst_node_2;
+  src_node_1->docID = 1;
+
+  src_node_2->next = NULL;
+  src_node_2->docID = 2;
+
+  /*traverse the llc linked_list*/
+
+  dsa_llc_realloc(dst_node_1, src_node_1, sizeof(node));
+  dsa_llc_realloc(dst_node_2, src_node_2, sizeof(node));
+
   return 0;
 }
