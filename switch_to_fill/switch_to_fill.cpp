8d0bd7112f3e691cfe92247ac79abafc5c1c645e
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-20 22:02:17 -0500

    accept / reject offload

--- switch_to_fill/switch_to_fill.cpp
+++ switch_to_fill/switch_to_fill.cpp
@@ -1,4 +1,5 @@
 #include "timer_utils.h"
+#include <cstdlib>
 #include <atomic>
 /*
   End-to-End Evaluation
@@ -14,7 +15,7 @@
   - preallocated buffers: as long as we know where to access them for post-processing
 
 
-  8:57 - 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 - 9:44
+
 
   Switch-To-Fill Scheduling:
   - Count completed requests to know when to terminate
@@ -31,6 +32,7 @@
 
   submit_flag <- while this is set an offload is waiting to be observed
 
+  we need a copy of the host's descriptor from inside the portal
 */
 #define STATUS_SUCCESS 0
 #define STATUS_FAIL 1
@@ -40,17 +42,25 @@ std::atomic<int> submit_flag;
 std::atomic<int> submit_status;
 typedef struct _offload_entry{
   uint64_t start_time;
+  uint64_t comp_time;
   struct _offload_entry *next;
 } offload_entry;
 typedef struct _ax_params {
   int max_inflights;
+  uint64_t offload_time;
 } ax_params;
 
 
 void nonblocking_emul_ax(void *arg){
+  ax_params *args = (ax_params *)arg;
   bool offload_in_flight = false;
   offload_entry *in_flight_ents = NULL;
+  offload_entry *last_offload_ent = NULL;
   uint64_t next_offload_completion_time = 0;
+  int max_inflight = args->max_inflights;
+  uint64_t offload_time = args->offload_time;
+  int in_flight = 0;
+
   if(offload_in_flight){
     /*
       if time is up for the earliest submitted offload
@@ -59,6 +69,11 @@ void nonblocking_emul_ax(void *arg){
         make sure we let ourselves know
         make sure we start checking the current time against the time for the next earliest submitted offload
     */
+    in_flight_ents = in_flight_ents->next;
+    if( in_flight_ents != NULL ){
+      offload_in_flight = true;
+      next_offload_completion_time = in_flight_ents->comp_time;
+    }
 
   }
   if(submit_flag.load() == OFFLOAD_REQUESTED){
@@ -69,7 +84,20 @@ void nonblocking_emul_ax(void *arg){
     else:
       notify submitter of failure
     */
-    submit_flag = OFFLOAD_RECEIVED; /*received submission*/
+    if(in_flight < max_inflight ){
+      submit_status = STATUS_SUCCESS;
+      submit_flag = OFFLOAD_RECEIVED; /*received submission*/
+      offload_entry *new_ent = (offload_entry *)malloc(sizeof(offload_entry));
+      new_ent->start_time = start_time;
+      new_ent->comp_time = start_time + offload_time;
+      last_offload_ent->next = new_ent;
+      last_offload_ent = new_ent;
+      in_flight++;
+    } else {
+      submit_status = STATUS_FAIL;
+      submit_flag = OFFLOAD_RECEIVED; /*received submission*/
+    }
+
   }
 }
 
