696fb1f27ff86a747f02ee7c14ed4187e02e7b51
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-20 22:58:54 -0500

    create thread pinned

--- switch_to_fill/switch_to_fill.cpp
+++ switch_to_fill/switch_to_fill.cpp
@@ -1,6 +1,12 @@
 #include "timer_utils.h"
+#include <pthread.h>
+#include "print_utils.h"
+#include "status.h"
 #include <cstdlib>
 #include <atomic>
+#include <list>
+#include <cstring>
+
 /*
   End-to-End Evaluation
   - Blocking vs. Switch to Fill
@@ -13,9 +19,6 @@
 
   Output generation/post-processing:
   - preallocated buffers: as long as we know where to access them for post-processing
-
-
-
 
   Switch-To-Fill Scheduling:
   - Count completed requests to know when to terminate
@@ -30,27 +33,37 @@
 
   emul_non_blocking_ax -- meets offload times we specify?
 
-  submit_flag <- while this is set an offload is waiting to be observed
-
-  we need a copy of the host's descriptor from inside the portal
 */
-#define STATUS_SUCCESS 0
-#define STATUS_FAIL 1
+#define SUBMIT_SUCCESS 0
+#define SUBMIT_FAIL 1
 #define OFFLOAD_RECEIVED 1
 #define OFFLOAD_REQUESTED 0
 #define COMP_STATUS_COMPLETED 1
 #define COMP_STATUS_PENDING 0
 std::atomic<int> submit_flag;
 std::atomic<int> submit_status;
+std::atomic<uint64_t> compl_addr;
+bool gDebugParam = false;
 typedef struct completion_record{
   int status;
 } ax_comp;
-typedef struct _offload_entry{
+class offload_entry{
+  public:
+  offload_entry(){
+    start_time = 0;
+    comp_time = 0;
+    comp = NULL;
+  }
+  offload_entry(uint64_t start, uint64_t compl_time, ax_comp *c){
+    start_time = start;
+    comp_time = compl_time;
+    comp = c;
+  }
   uint64_t start_time;
   uint64_t comp_time;
-  struct _offload_entry *next;
+  int32_t id;
   ax_comp *comp;
-} offload_entry;
+};
 typedef struct _desc{
   int32_t id;
   ax_comp *comp;
@@ -61,12 +74,79 @@ typedef struct _ax_params {
   uint64_t offload_time;
 } ax_params;
 
+int create_thread_pinned(pthread_t *thread, void *(*func)(void*), void *arg, int coreId){
+    pthread_attr_t attr;
+  cpu_set_t cpuset;
+  struct sched_param param;
 
-void nonblocking_emul_ax(void *arg){
+  int status = pthread_attr_init(&attr);
+
+  if(status != 0){
+    PRINT_DBG( "Error initializing thread attributes\n");
+    return STATUS_FAIL;
+  }
+
+  CPU_ZERO(&cpuset);
+  CPU_SET(coreId, &cpuset);
+  status = pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset);
+  if(status != 0){
+    PRINT_DBG( "Error setting thread affinity\n");
+    pthread_attr_destroy(&attr);
+    return STATUS_FAIL;
+  }
+
+  status = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+  if(status != 0){
+    PRINT_DBG( "Error setting thread scheduling inheritance\n");
+    pthread_attr_destroy(&attr);
+    return STATUS_FAIL;
+  }
+
+  if (pthread_attr_setschedpolicy(
+        &attr, SCHED_RR) != 0)
+  {
+      PRINT_DBG(
+              "Failed to set scheduling policy for thread!\n");
+      pthread_attr_destroy(&attr);
+      return STATUS_FAIL;
+  }
+
+  memset(&param, 0, sizeof(param));
+  param.sched_priority = 15;
+  if (pthread_attr_setschedparam(&attr, &param) != 0)
+  {
+      PRINT_DBG(
+              "Failed to set the sched parameters attribute!\n");
+      pthread_attr_destroy(&attr);
+      return STATUS_FAIL;
+  }
+
+  if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE) != 0)
+  {
+      PRINT_DBG(
+              "Failed to set the dettachState attribute!\n");
+      pthread_attr_destroy(&attr);
+      return STATUS_FAIL;
+  }
+  if (pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0)
+  {
+      PRINT_DBG("Failed to set the attribute!\n");
+      pthread_attr_destroy(&attr);
+      return STATUS_FAIL;
+  }
+  if(pthread_create(thread, &attr, func, arg) != 0){
+    PRINT_DBG( "Error creating thread\n");
+    PRINT_DBG( "Creating thread with NULL attributes\n");
+    pthread_create(thread, NULL, func, arg);
+  }
+  return STATUS_SUCCESS;
+}
+
+
+void *nonblocking_emul_ax(void *arg){
   ax_params *args = (ax_params *)arg;
   bool offload_in_flight = false;
-  offload_entry *earliest_inflight_ent = NULL;
-  offload_entry *last_offload_ent = NULL;
+  std::list<offload_entry*> offload_in_flight_list;
   uint64_t next_offload_completion_time = 0;
   int max_inflight = args->max_inflights;
   uint64_t offload_time = args->offload_time;
@@ -80,8 +160,10 @@ void nonblocking_emul_ax(void *arg){
     */
     uint64_t cur_time = sampleCoderdtsc();
     if(cur_time >= next_offload_completion_time){
+      next_completed_offload_comp = offload_in_flight_list.front()->comp;
       next_completed_offload_comp->status = COMP_STATUS_COMPLETED;
       in_flight--;
+      PRINT_DBG("Offload");
     }
 
     /*
@@ -89,10 +171,10 @@ void nonblocking_emul_ax(void *arg){
         make sure we let ourselves know
         make sure we start checking the current time against the time for the next earliest submitted offload
     */
-    earliest_inflight_ent = earliest_inflight_ent->next;
-    if( earliest_inflight_ent != NULL ){
+    offload_in_flight_list.pop_front();
+    if( ! offload_in_flight_list.empty() ){
       offload_in_flight = true;
-      next_offload_completion_time = earliest_inflight_ent->comp_time;
+      next_offload_completion_time = offload_in_flight_list.front()->comp_time;
     } else {
       offload_in_flight = false;
     }
@@ -107,24 +189,25 @@ void nonblocking_emul_ax(void *arg){
       notify submitter of failure
     */
     if(in_flight < max_inflight ){
-      submit_status = STATUS_SUCCESS;
+      submit_status = SUBMIT_SUCCESS;
+      struct completion_record *comp_addr = (struct completion_record *)compl_addr.load();
       submit_flag = OFFLOAD_RECEIVED; /*received submission*/
-      offload_entry *new_ent = (offload_entry *)malloc(sizeof(offload_entry));
-      new_ent->start_time = start_time;
-      new_ent->comp_time = start_time + offload_time;
-      last_offload_ent->next = new_ent;
-      last_offload_ent = new_ent;
+
+      offload_entry *new_ent = new offload_entry(start_time, start_time + offload_time, comp_addr);
       in_flight++;
     } else {
-      submit_status = STATUS_FAIL;
+      submit_status = SUBMIT_FAIL;
       submit_flag = OFFLOAD_RECEIVED; /*received submission*/
     }
 
   }
+  return NULL;
 }
 
 int main(){
-
+  pthread_t ax_td;
+  create_thread_pinned(&ax_td, nonblocking_emul_ax, NULL, 0);
+  pthread_join(ax_td, NULL);
 
   return 0;
 }
