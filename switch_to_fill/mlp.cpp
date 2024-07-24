7a7e114d735df8eb4f8c461e19202513a67e8ef7
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-23 22:38:23 -0500

    init aecs

--- switch_to_fill/mlp.cpp
+++ switch_to_fill/mlp.cpp
@@ -5,6 +5,7 @@ extern "C" {
   #include "fcontext.h"
   #include "iaa.h"
   #include "accel_test.h"
+  #include "iaa_compress.h"
 }
 
 int gLogLevel = LOG_MONITOR;
@@ -21,6 +22,12 @@ int main(){
   int itr = 1;
 
   int bufsize = 1024;
+
+  char *src1 = (char *) aligned_alloc(32,bufsize);
+  char *dst1 = (char *) aligned_alloc(32,IAA_COMPRESS_MAX_DEST_SIZE);
+  char *src2 =
+    (char *) aligned_alloc(32, IAA_COMPRESS_SRC2_SIZE);
+  memcpy(src2, iaa_compress_aecs, IAA_COMPRESS_AECS_SIZE);
 
   iaa = acctest_init(tflags);
   rc = acctest_alloc(iaa, wq_type, dev_id, wq_id);
@@ -40,6 +47,26 @@ int main(){
     }
     tsk_node = tsk_node->next;
   }
+  struct hw_desc *hw;
+  hw = (struct hw_desc *) malloc(sizeof(struct hw_desc));
+  memset(hw, 0, sizeof(struct hw_desc));
+  ax_comp *comp =
+    (ax_comp *) aligned_alloc(iaa->compl_size, sizeof(ax_comp));
+
+
+  hw->flags = 0x5000eUL;
+  hw->opcode = 0x43;
+  hw->src_addr = (uint64_t) src1;
+  hw->dst_addr = (uint64_t) dst1;
+  hw->xfer_size = bufsize;
+  hw->completion_addr = (uint64_t) comp;
+  hw->iax_compr_flags = 14;
+  hw->iax_src2_addr = (uint64_t) src2;
+  hw->iax_src2_xfer_size = IAA_COMPRESS_AECS_SIZE;
+  hw->iax_max_dst_size = bufsize;
+
+
+
   iaa_compress_multi_task_nodes(iaa);
   rc = iaa_task_result_verify_task_nodes(iaa, 0);
   acctest_free_task(iaa);
