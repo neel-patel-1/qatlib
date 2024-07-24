c7952f0f6d1cc02f9c570b7e18e094e12cbb278b
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-23 22:46:47 -0500

    memset pattern

--- switch_to_fill/mlp.cpp
+++ switch_to_fill/mlp.cpp
@@ -23,11 +23,14 @@ int main(){
 
   int bufsize = 1024;
 
+  uint64_t pattern = 0x98765432abcdef01;
   char *src1 = (char *) aligned_alloc(32,bufsize);
   char *dst1 = (char *) aligned_alloc(32,IAA_COMPRESS_MAX_DEST_SIZE);
   char *src2 =
     (char *) aligned_alloc(32, IAA_COMPRESS_SRC2_SIZE);
   memcpy(src2, iaa_compress_aecs, IAA_COMPRESS_AECS_SIZE);
+
+  memset_pattern(src1, pattern, bufsize);
 
   iaa = acctest_init(tflags);
   rc = acctest_alloc(iaa, wq_type, dev_id, wq_id);
@@ -59,16 +62,19 @@ int main(){
   hw->src_addr = (uint64_t) src1;
   hw->dst_addr = (uint64_t) dst1;
   hw->xfer_size = bufsize;
+
   hw->completion_addr = (uint64_t) comp;
   hw->iax_compr_flags = 14;
   hw->iax_src2_addr = (uint64_t) src2;
   hw->iax_src2_xfer_size = IAA_COMPRESS_AECS_SIZE;
   hw->iax_max_dst_size = bufsize;
 
+  acctest_desc_submit(iaa, hw);
 
+  acctest_wait_on_desc_timeout(comp, iaa, 1000);
 
-  iaa_compress_multi_task_nodes(iaa);
-  rc = iaa_task_result_verify_task_nodes(iaa, 0);
+  // iaa_compress_multi_task_nodes(iaa);
+  // rc = iaa_task_result_verify_task_nodes(iaa, 0);
   acctest_free_task(iaa);
   acctest_free(iaa);
 }
