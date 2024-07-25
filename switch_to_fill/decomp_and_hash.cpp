f5ad024fb10877013f22a3bd7938e9c32921b65f
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-24 19:49:28 -0500

    decomp and hash

---
+++ switch_to_fill/decomp_and_hash.cpp
@@ -0,0 +1,55 @@
+#include "test_harness.h"
+#include "print_utils.h"
+#include "router_request_args.h"
+#include "iaa_offloads.h"
+#include "ch3_hash.h"
+#include "runners.h"
+#include "gpcore_compress.h"
+
+extern "C" {
+  #include "fcontext.h"
+  #include "iaa.h"
+  #include "accel_test.h"
+  #include "iaa_compress.h"
+  #include <zlib.h>
+}
+#include "decompress_and_hash_request.hpp"
+
+
+int gLogLevel = LOG_PERF;
+bool gDebugParam = false;
+int main(){
+
+
+  int wq_id = 0;
+  int dev_id = 3;
+  int wq_type = SHARED;
+  int rc;
+  int itr = 1000;
+  int total_requests = 1000;
+  initialize_iaa_wq(dev_id, wq_id, wq_type);
+
+  run_gpcore_request_brkdown(
+    cpu_decompress_and_hash_stamped,
+    compressed_mc_req_allocator,
+    compressed_mc_req_free,
+    itr,
+    total_requests
+  );
+  run_blocking_offload_request_brkdown(
+    blocking_decompress_and_hash_request_stamped,
+    alloc_decomp_and_hash_offload_args,
+    free_decomp_and_hash_offload_args,
+    itr,
+    total_requests
+  );
+  run_yielding_request_brkdown(
+    yielding_decompress_and_hash_request_stamped,
+    alloc_decomp_and_hash_offload_args,
+    free_decomp_and_hash_offload_args,
+    itr,
+    total_requests
+  );
+
+  free_iaa_wq();
+}
