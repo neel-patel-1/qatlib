a20f9e602cf3604fb7b2c162e4dcc0feb9e80033
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-25 22:26:28 -0500

    yielding request functinos

--- switch_to_fill/decomp_and_hash_multi_threaded.cpp
+++ switch_to_fill/decomp_and_hash_multi_threaded.cpp
@@ -106,6 +106,107 @@ void run_blocking_offered_load_synchronized(
   );
 }
 
+void yielding_request_offered_load_synchronized(
+  fcontext_fn_t request_fn,
+  void (* offload_args_allocator)(int, offload_request_args***, ax_comp *comps),
+  void (* offload_args_free)(int, offload_request_args***),
+  int requests_sampling_interval,
+  int total_requests,
+  uint64_t *exetime, int idx,
+  pthread_barrier_t *test_start_barrier,
+  pthread_barrier_t *tear_down_barrier
+)
+{
+  using namespace std;
+  fcontext_state_t *self = fcontext_create_proxy();
+  char**dst_bufs;
+  ax_comp *comps;
+  offload_request_args **off_args;
+  fcontext_transfer_t *offload_req_xfer;
+  fcontext_state_t **off_req_state;
+
+  int sampling_intervals = (total_requests / requests_sampling_interval);
+  int sampling_interval_timestamps = sampling_intervals + 1;
+  uint64_t sampling_interval_completion_times[sampling_interval_timestamps];
+
+
+  requests_completed = 0;
+
+  /* Pre-allocate the CRs */
+  allocate_crs(total_requests, &comps);
+
+  /* allocate request args */
+  offload_args_allocator(total_requests, &off_args, comps);
+
+  /* Pre-create the contexts */
+  offload_req_xfer = (fcontext_transfer_t *)malloc(sizeof(fcontext_transfer_t) * total_requests);
+  off_req_state = (fcontext_state_t **)malloc(sizeof(fcontext_state_t *) * total_requests);
+
+  create_contexts(off_req_state, total_requests, request_fn);
+
+  pthread_barrier_wait(test_start_barrier);
+
+  execute_yielding_requests_closed_system_with_sampling(
+    requests_sampling_interval, total_requests,
+    sampling_interval_completion_times, sampling_interval_timestamps,
+    comps, off_args,
+    offload_req_xfer, off_req_state, self,
+    exetime, idx);
+
+  /* teardown */
+  free_contexts(off_req_state, total_requests);
+  free(offload_req_xfer);
+  free(comps);
+
+  offload_args_free(total_requests, &off_args);
+
+  pthread_barrier_wait(tear_down_barrier);
+
+  fcontext_destroy(self);
+}
+
+
+void run_yielding_offered_load_synchronized(
+  fcontext_fn_t request_fn,
+  void (* offload_args_allocator)(int, offload_request_args***, ax_comp *comps),
+  void (* offload_args_free)(int, offload_request_args***),
+  int num_exe_time_samples_per_run,
+  int total_requests, int iter,
+  pthread_barrier_t *test_start_barrier,
+  pthread_barrier_t *tear_down_barrier){
+
+  /*
+    we take exe time samples periodically as a fixed number of requests complete
+    This enables discarding results collected during the latter phase of the test
+    where the system has ran out of work to execute during blocking stalls
+  */
+  int num_requests_before_stamp = total_requests / num_exe_time_samples_per_run;
+  int total_exe_time_samples = iter * num_exe_time_samples_per_run;
+  uint64_t exetime[total_exe_time_samples];
+
+  /* this function takes multiple samples */
+  for(int i = 0; i < iter; i++){
+    yielding_request_offered_load_synchronized(
+      request_fn,
+      offload_args_allocator,
+      offload_args_free,
+      num_requests_before_stamp,
+      total_requests,
+      exetime,
+      i * num_exe_time_samples_per_run,
+      test_start_barrier,
+      tear_down_barrier
+    );
+  }
+
+  for(int i=0; i<total_exe_time_samples; i++){
+    LOG_PRINT(LOG_DEBUG, "ExeTime: %lu\n", exetime[i]);
+  }
+  mean_median_stdev_rps(
+    exetime, total_exe_time_samples, num_requests_before_stamp, "SwitchToFillRPS"
+  );
+}
+
 enum runner_type {
   GPCORE,
   BLOCKING,
@@ -128,6 +229,8 @@ typedef struct _runner_thread_args{
 
   pthread_barrier_t *test_start_barrier;
   pthread_barrier_t *tear_down_barrier;
+
+  int num_exe_time_samples_per_run;
 } runner_thread_args;
 
 void *runner_thread_fn(void *arg){
@@ -161,17 +264,59 @@ void *runner_thread_fn(void *arg){
       );
       break;
     case YIELDING:
-      // run_yielding_request_brkdown(
-      //   request_fn,
-      //   args->offload_args_allocator,
-      //   args->offload_args_free,
-      //   itr,
-      //   total_requests
-      // );
+      run_yielding_offered_load_synchronized(
+        request_fn,
+        args->offload_args_allocator,
+        args->offload_args_free,
+        args->num_exe_time_samples_per_run,
+        total_requests,
+        itr,
+        test_start_barrier,
+        tear_down_barrier
+      );
       break;
   }
 
   return NULL;
+}
+
+void start_multi_thread_test(
+  fcontext_fn_t request_fn,
+  void (* offload_args_allocator)(int, offload_request_args***, ax_comp *comps),
+  void (* offload_args_free)(int, offload_request_args***),
+  int total_requests,
+  int num_exe_time_samples_per_run,
+  int itr,
+  int num_workers
+){
+  runner_thread_args args;
+  int cores[num_workers];
+  pthread_t threads[num_workers];
+  pthread_barrier_t test_start_barrier;
+  pthread_barrier_t tear_down_barrier;
+
+  for(int i=0; i<num_workers; i++){
+    cores[i] = i;
+  }
+
+  pthread_barrier_init(&test_start_barrier, NULL, num_workers);
+  pthread_barrier_init(&tear_down_barrier, NULL, num_workers);
+  for(int i=0; i<num_workers; i++){
+    args.request_fn = request_fn;
+    args.offload_args_allocator = offload_args_allocator;
+    args.offload_args_free = offload_args_free;
+    args.runner_type = BLOCKING;
+    args.itr = itr;
+    args.num_exe_time_samples_per_run = num_exe_time_samples_per_run;
+    args.test_start_barrier = &test_start_barrier;
+    args.tear_down_barrier = &tear_down_barrier;
+    create_thread_pinned(&(threads[i]), runner_thread_fn, &args, cores[i]);
+  }
+  for(auto thread: threads){
+    pthread_join(thread, NULL);
+  }
+  pthread_barrier_destroy(&test_start_barrier);
+  pthread_barrier_destroy(&tear_down_barrier);
 }
 
 int gLogLevel = LOG_PERF;
@@ -188,16 +333,15 @@ int main(){
 
   std::string append_string = query;
 
-  runner_thread_args args;
+
   int num_workers = 4;
   int cores[num_workers];
   pthread_t threads[num_workers];
   pthread_barrier_t test_start_barrier;
   pthread_barrier_t tear_down_barrier;
   int query_size = 65536; /* testing a single large size -- trigger software fallback*/
+  runner_thread_args *args;
 
-  args.itr = itr;
-  args.total_requests = total_requests;
 
   for(int i=0; i<num_workers; i++){
     cores[i] = i;
@@ -212,14 +356,17 @@ int main(){
   pthread_barrier_init(&test_start_barrier, NULL, num_workers);
   pthread_barrier_init(&tear_down_barrier, NULL, num_workers);
   for(int i=0; i<num_workers; i++){
-    args.request_fn = blocking_decompress_and_hash_request;
-    args.offload_args_allocator = alloc_decomp_and_hash_offload_args;
-    args.offload_args_free = free_decomp_and_hash_offload_args;
-    args.runner_type = BLOCKING;
-    args.itr = itr;
-    args.test_start_barrier = &test_start_barrier;
-    args.tear_down_barrier = &tear_down_barrier;
-    rc = create_thread_pinned(&(threads[i]), runner_thread_fn, &args, cores[i]);
+    args = (runner_thread_args *)malloc(sizeof(runner_thread_args));
+    args->itr = itr;
+    args->total_requests = total_requests;
+    args->request_fn = blocking_decompress_and_hash_request;
+    args->offload_args_allocator = alloc_decomp_and_hash_offload_args;
+    args->offload_args_free = free_decomp_and_hash_offload_args;
+    args->runner_type = BLOCKING;
+    args->itr = itr;
+    args->test_start_barrier = &test_start_barrier;
+    args->tear_down_barrier = &tear_down_barrier;
+    rc = create_thread_pinned(&(threads[i]), runner_thread_fn, args, cores[i]);
     if (rc){
       LOG_PRINT(LOG_ERR, "Error creating thread\n");
       exit(-1);
@@ -230,6 +377,15 @@ int main(){
   }
   pthread_barrier_destroy(&test_start_barrier);
   pthread_barrier_destroy(&tear_down_barrier);
+
+  // start_multi_thread_test(
+  //   blocking_decompress_and_hash_request,
+  //   alloc_decomp_and_hash_offload_args,
+  //   free_decomp_and_hash_offload_args,
+  //   total_requests,
+  //   -1,
+  //   itr,
+  //   num_workers);
 
 
     // run_gpcore_request_brkdown(
