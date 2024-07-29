d8bbc863963fa86c41a409fa37fd7d97860d7209
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-29 07:23:29 -0500

    best case yielding breakdown

--- switch_to_fill/decomp_and_hash.cpp
+++ switch_to_fill/decomp_and_hash.cpp
@@ -15,6 +15,134 @@ extern "C" {
 }
 #include "decompress_and_hash_request.hpp"
 
+void execute_yielding_requests_best_case(
+  int total_requests,
+  ax_comp *comps, timed_offload_request_args **off_args,
+  fcontext_transfer_t *offload_req_xfer,
+  fcontext_state_t **off_req_state,
+  fcontext_state_t *self,
+  uint64_t *off_times, uint64_t *yield_to_resume_times, uint64_t *hash_times, int idx)
+{
+
+  /*
+  while(reqs < total)
+  xfer = start_yielding_req
+  wait_for_yielding_reqs_comp
+  resume_yielding_req(xfer)
+  */
+  while(requests_completed < total_requests){
+    offload_req_xfer[requests_completed] =
+        fcontext_swap(off_req_state[requests_completed]->context, off_args[requests_completed]);
+    while(comps[requests_completed].status == COMP_STATUS_COMPLETED){
+      _mm_pause();
+    }
+    fcontext_swap(offload_req_xfer[requests_completed].prev_context, NULL);
+    LOG_PRINT(LOG_DEBUG, "Request %d completed\n", requests_completed);
+  }
+
+  /* get the time stamps from one of the requests */
+  uint64_t *ts0 = off_args[0]->ts0;
+  uint64_t *ts1 = off_args[0]->ts1;
+  uint64_t *ts2 = off_args[0]->ts2;
+  uint64_t *ts3 = off_args[0]->ts3;
+  uint64_t diff[total_requests];
+  avg_samples_from_arrays(diff, off_times[idx], ts1, ts0, requests_completed);
+  LOG_PRINT( LOG_DEBUG, "Offload time: %lu\n", off_times[idx]);
+  avg_samples_from_arrays(diff, yield_to_resume_times[idx], ts2, ts1, requests_completed);
+  LOG_PRINT( LOG_DEBUG, "YieldToResumeDelay: %lu\n", yield_to_resume_times[idx]);
+  avg_samples_from_arrays(diff, hash_times[idx], ts3, ts2, requests_completed);
+  LOG_PRINT( LOG_DEBUG, "HashTime: %lu\n", hash_times[idx]);
+}
+
+void yielding_best_case_request_breakdown(
+  fcontext_fn_t request_fn,
+  void (* offload_args_allocator)
+    (int, timed_offload_request_args***,
+      ax_comp *comps, uint64_t *ts0,
+      uint64_t *ts1, uint64_t *ts2,
+      uint64_t *ts3),
+  void (* offload_args_free)(int, timed_offload_request_args***),
+  int total_requests,
+  uint64_t *offload_time, uint64_t *wait_time, uint64_t *kernel2_time, int idx
+){
+  using namespace std;
+  fcontext_state_t *self = fcontext_create_proxy();
+  char**dst_bufs;
+  ax_comp *comps;
+  timed_offload_request_args **off_args;
+  fcontext_transfer_t *offload_req_xfer;
+  fcontext_state_t **off_req_state;
+
+  int requests_sampling_interval = total_requests;
+  int sampling_intervals = (total_requests / requests_sampling_interval);
+  int sampling_interval_timestamps = sampling_intervals + 1;
+  uint64_t *ts0, *ts1, *ts2, *ts3;
+
+  ts0 = (uint64_t *)malloc(sizeof(uint64_t) * total_requests);
+  ts1 = (uint64_t *)malloc(sizeof(uint64_t) * total_requests);
+  ts2 = (uint64_t *)malloc(sizeof(uint64_t) * total_requests);
+  ts3 = (uint64_t *)malloc(sizeof(uint64_t) * total_requests);
+
+  requests_completed = 0;
+
+  /* Pre-allocate the CRs */
+  allocate_crs(total_requests, &comps);
+
+  /* allocate request args */
+  offload_args_allocator(total_requests, &off_args, comps,
+    ts0, ts1, ts2, ts3);
+
+  /* Pre-create the contexts */
+  offload_req_xfer = (fcontext_transfer_t *)malloc(sizeof(fcontext_transfer_t) * total_requests);
+  off_req_state = (fcontext_state_t **)malloc(sizeof(fcontext_state_t *) * total_requests);
+
+  create_contexts(off_req_state, total_requests, request_fn);
+
+  execute_yielding_requests_best_case(
+    total_requests,
+    comps, off_args,
+    offload_req_xfer, off_req_state, self,
+    offload_time, wait_time, kernel2_time, idx);
+
+  /* teardown */
+  free_contexts(off_req_state, total_requests);
+  free(offload_req_xfer);
+  free(comps);
+
+  offload_args_free(total_requests, &off_args);
+
+  fcontext_destroy(self);
+}
+
+void run_yielding_best_case_request_brkdown(
+  fcontext_fn_t req_fn,
+  void (* offload_args_allocator)
+    (int, timed_offload_request_args***,
+      ax_comp *comps, uint64_t *ts0,
+      uint64_t *ts1, uint64_t *ts2,
+      uint64_t *ts3),
+  void (* offload_args_free)(int, timed_offload_request_args***),
+  int iter, int total_requests
+){
+  uint64_t *offloadtime;
+  uint64_t *waittime, *posttime;
+  offloadtime = (uint64_t *)malloc(sizeof(uint64_t) * iter);
+  waittime = (uint64_t *)malloc(sizeof(uint64_t) * iter);
+  posttime = (uint64_t *)malloc(sizeof(uint64_t) * iter);
+  for(int i=0; i<iter; i++){
+    yielding_best_case_request_breakdown(
+      req_fn,
+      offload_args_allocator,
+      offload_args_free,
+      total_requests,
+      offloadtime, waittime, posttime, i
+    );
+  }
+  print_mean_median_stdev(offloadtime, iter, "OffloadSwitchToFill");
+  print_mean_median_stdev(waittime, iter, "YieldToResume");
+  print_mean_median_stdev(posttime, iter, "PostProcessingSwitchToFill");
+}
+
 
 int gLogLevel = LOG_PERF;
 bool gDebugParam = false;
@@ -69,6 +197,13 @@ int main(int argc, char **argv){
     );
     run_blocking_offload_request_brkdown(
       blocking_decompress_and_hash_request_stamped,
+      alloc_decomp_and_hash_offload_args_stamped,
+      free_decomp_and_hash_offload_args_stamped,
+      itr,
+      total_requests
+    );
+    run_yielding_best_case_request_brkdown(
+      yielding_decompress_and_hash_request_stamped,
       alloc_decomp_and_hash_offload_args_stamped,
       free_decomp_and_hash_offload_args_stamped,
       itr,
