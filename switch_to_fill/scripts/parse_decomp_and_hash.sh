53499acbb52f13b60330a5800d40c231ee6343a8
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-28 21:42:45 -0500

    run and parse decomp and hash

---
+++ switch_to_fill/scripts/parse_decomp_and_hash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source configs/phys_core.sh
+
+QUERY_SIZES=( 64    256  1024 4096 16384 65536 262144 1048576 )
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+
+for q in ${QUERY_SIZES[@]}; do
+  grep -v main decomp_and_hash_logs/core_${CORE}_querysize_${q}.log \
+    | grep -v info \
+    | grep -v RPS \
+    | awk '/Kernel1/{printf("%s", " 0 0 ") } /Offload/{printf(" ");} {printf("%s ", $5);} /Kernel2/{printf("\n")  } /Wait/{printf("%s", "0 ");} /Post/{print " "}'
+done
---
+++ switch_to_fill/scripts/run_decomp_and_hash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# sudo python3 scripts/accel_conf.py --load=configs/iaa-1n1d8e1w128q-s-n2.conf
+source configs/phys_core.sh
+source configs/devid.sh
+
+QUERY_SIZES=( 256  64  1024 4096 16384 65536 262144 1048576 )
+ITERATIONS=(  10   10  10  10  10   10    10     10  )
+REQUESTS=(    100 100 100 100 100  10   10    10 )
+
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+[ -z "$DEVID" ] && echo "DEVID is not set" && exit 1
+
+# Initialize decomp_and_hash_logs
+mkdir -p decomp_and_hash_logs
+for q in "${QUERY_SIZES[@]}"; do
+    echo -n > decomp_and_hash_logs/core_${CORE}_querysize_${q}.log
+done
+
+qidx=0
+for q in "${QUERY_SIZES[@]}"; do
+
+    #iterations corresponding to current the query size
+    iters=${ITERATIONS[$j]}
+    taskset -c ${CORE} sudo ./decomp_and_hash \
+        -d $DEVID \
+        -q $q \
+        -t ${REQUESTS[$qidx]} \
+        -i $iters >> decomp_and_hash_logs/core_${CORE}_querysize_${q}.log &
+    wait
+    qidx=$((qidx+1))
+done
