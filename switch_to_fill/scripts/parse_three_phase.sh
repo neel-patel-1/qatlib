31b425853caf5c60995b5984d45836df883a7ced
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-30 13:05:29 -0500

    run and parse three phase

---
+++ switch_to_fill/scripts/parse_three_phase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source configs/phys_core.sh
+
+QUERY_SIZES=( 64    256  1024 4096 16384 65536 262144 1048576 )
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+
+for q in ${QUERY_SIZES[@]}; do
+  grep -v main three_phase_logs/core_${CORE}_querysize_${q}.log \
+    | grep -v info \
+    | grep -v RPS \
+    | awk '/Kernel1/{printf("%s", " 0 ") } \
+     /Offload/{printf(" ");} \
+      {printf("%s ", $5);} /Kernel2/{printf("\n")  } \
+      /Post/{print " "}'
+done
