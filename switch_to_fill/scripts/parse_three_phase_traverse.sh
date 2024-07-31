ea9f7d1b553c1f4a437d5ad90e88032b55160ff6
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-30 23:52:01 -0500

    backup old parse scripts

---
+++ switch_to_fill/scripts/parse_three_phase_traverse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source configs/phys_core.sh
+
+QUERY_SIZES=( 64    256  1024 4096 16384 $((42 * 1024)) 65536 262144 1048576 )
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+
+for q in ${QUERY_SIZES[@]}; do
+  grep -v main three_phase_logs/core_${CORE}_querysize_${q}.log \
+    | grep -v info \
+    | grep -v RPS \
+    | awk "BEGIN{printf(\"${q} \");}\
+      /Offload Mean/{printf(\"BlockingOffload %s \", \$5 );} \
+      /Wait Mean/{printf(\"%s \", \$5);  } \
+      /Post Mean/{printf(\"%s\n\", \$5);} \
+      /OffloadSwitchToFill/{printf(\" SwitchToFill %s \", \$5);} \
+      /YieldToResume/{printf(\"%s \", \$5);} \
+      /PostProcessingSwitchToFill/{printf(\"%s\n\", \$5);} \
+      "
+    echo
+done
---
+++ switch_to_fill/scripts/run_three_phase_travers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# sudo python3 scripts/accel_conf.py --load=configs/iaa-1n1d8e1w128q-s-n2.conf
+
+source configs/phys_core.sh
+source configs/devid.sh
+
+# QUERY_SIZES=( 256  64  1024 4096 16384 65536 262144 1048576 )
+# ITERATIONS=(  10   10  10  10  10   10    10     10  )
+# REQUESTS=(    100 100 100 100 100  10   10    10 )
+QUERY_SIZES=( $((42 * 1024)) )
+ITERATIONS=(  100  )
+REQUESTS=(    100 )
+
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+[ -z "$DEVID" ] && echo "DEVID is not set" && exit 1
+NUM_ACCESSES=10
+
+# Initialize three_phase_logs
+mkdir -p three_phase_logs
+for q in "${QUERY_SIZES[@]}"; do
+    echo -n > three_phase_logs/core_${CORE}_querysize_${q}.log
+done
+
+qidx=0
+for q in "${QUERY_SIZES[@]}"; do
+    taskset -c $CORE sudo ./three_phase \
+        -d $DSA_DEV_ID \
+        -q $q \
+        -t ${REQUESTS[$qidx]} \
+        -i ${ITERATIONS[$qidx]} >> three_phase_logs/core_${CORE}_querysize_${q}.log
+    qidx=$((qidx+1))
+done
