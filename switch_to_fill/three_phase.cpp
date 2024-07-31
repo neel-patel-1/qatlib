840ff814673f5d3c6a843c37668e1a951622dddd
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-07-31 00:41:22 -0500

    linear access comparison

--- switch_to_fill/three_phase.cpp
+++ switch_to_fill/three_phase.cpp
@@ -234,6 +234,19 @@ void alloc_offload_linear_access_args(
   *p_off_args = off_args;
 }
 
+void free_offload_linear_access_args(
+  int total_requests,
+  timed_offload_request_args*** p_off_args
+){
+  timed_offload_request_args **off_args = *p_off_args;
+  for(int i = 0; i < total_requests; i++){
+    free(off_args[i]->src_payload);
+    free(off_args[i]->dst_payload);
+    free(off_args[i]);
+  }
+  free(off_args);
+}
+
 void free_offload_serialized_access_args(
   int total_requests,
   timed_offload_request_args*** p_off_args
@@ -328,7 +341,7 @@ void yielding_offload_and_linear_access_stamped(fcontext_transfer_t arg){
   ts2[id] = sampleCoderdtsc();
 
   LOG_PRINT(LOG_DEBUG, "%d HostBytesAccesses\n" , host_bytes_accessed);
-  for(int i=0; i < pre_buf_size; i+=64){
+  for(int i=0; i < host_bytes_accessed; i+=64){
     pre_buf[i] = i;
   }
   LOG_PRINT(LOG_DEBUG, "%d AXBytesAccesses\n" , ax_bytes_accessed);
@@ -342,6 +355,59 @@ void yielding_offload_and_linear_access_stamped(fcontext_transfer_t arg){
   fcontext_swap(arg.prev_context, NULL);
 
 }
+
+void blocking_offload_and_linear_access_stamped(fcontext_transfer_t arg){
+  timed_offload_request_args *args = (timed_offload_request_args *)arg.data;
+ char *pre_buf = (char *)args->src_payload;
+ char *ax_dst_buf = (char *)args->dst_payload;
+ int pre_buf_size = input_size ;
+ int ax_bytes_accessed = args->dst_size;
+ int host_bytes_accessed = args->src_size;
+
+  int id = args->id;
+  uint64_t *ts0 = args->ts0;
+  uint64_t *ts1 = args->ts1;
+  uint64_t *ts2 = args->ts2;
+  uint64_t *ts3 = args->ts3;
+  ax_comp *comp = args->comp;
+  struct hw_desc *desc = args->desc;
+
+
+ /*access the whole host buf to start*/
+  ts0[id] = sampleCoderdtsc();
+
+  for(int i=0; i < pre_buf_size; i+=64){
+    pre_buf[i] = i;
+  }
+
+  prepare_dsa_memcpy_desc_with_preallocated_comp(
+    desc, (uint64_t)pre_buf, (uint64_t)ax_dst_buf,
+    (uint64_t)args->comp, (uint64_t)input_size
+  );
+  blocking_dsa_submit(dsa, desc);
+
+  ts1[id] = sampleCoderdtsc();
+
+  spin_on(comp);
+
+  ts2[id] = sampleCoderdtsc();
+
+  LOG_PRINT(LOG_DEBUG, "%d HostBytesAccesses\n" , host_bytes_accessed);
+  for(int i=0; i < host_bytes_accessed; i+=64){
+    pre_buf[i] = i;
+  }
+  LOG_PRINT(LOG_DEBUG, "%d AXBytesAccesses\n" , ax_bytes_accessed);
+  for(int i=0; i < ax_bytes_accessed; i+=64){
+    ax_dst_buf[i] = i;
+  }
+
+  ts3[id] = sampleCoderdtsc();
+
+  requests_completed ++;
+  fcontext_swap(arg.prev_context, NULL);
+
+}
+
 
 void yielding_offload_and_serial_access_stamped(fcontext_transfer_t arg){
   timed_offload_request_args *args = (timed_offload_request_args *)arg.data;
@@ -491,6 +557,34 @@ int main(int argc, char **argv){
 
   LOG_PRINT(LOG_PERF, "Input size: %d\n", input_size);
   if(!no_latency){
+
+    run_blocking_offload_request_brkdown(
+      blocking_offload_and_linear_access_stamped,
+      alloc_offload_linear_access_args,
+      free_offload_linear_access_args,
+      itr,
+      total_requests
+    );
+
+    run_yielding_interleaved_request_brkdown(
+      yielding_offload_and_linear_access_stamped,
+      hash_interleaved, /* upper bound on yield perf */
+      alloc_offload_linear_access_args,
+      free_offload_linear_access_args,
+      itr,
+      total_requests
+    );
+
+    run_yielding_interleaved_request_brkdown(
+      yielding_offload_and_linear_access_stamped,
+      antagonist_interleaved, /* upper bound on yield perf */
+      alloc_offload_linear_access_args,
+      free_offload_linear_access_args,
+      itr,
+      total_requests
+    );
+
+    return 0;
     run_blocking_offload_request_brkdown(
       blocking_offload_and_access_stamped,
       alloc_offload_serialized_access_args,
